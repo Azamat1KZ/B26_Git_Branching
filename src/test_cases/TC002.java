bbc0631a4e0ee952efd5a93cc3f6ae5a948ee1bc
Azamat1KZ <98862639+Azamat1KZ@users.noreply.github.com>
2022-04-02 14:40:16 -0400

    updated test case 002

--- src/test_cases/TC002.java
+++ src/test_cases/TC002.java
@@ -4,5 +4,7 @@ public class TC002 {
     public static void main(String[] args) {
         //test case two
         // writing smoke tests...
+        // creating a report
+        // screenshots in failure
     }
 }
